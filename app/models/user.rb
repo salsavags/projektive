3f771a7b6f5a814a92a7ef3964690a80f7fbb3c0
Ian Johnson <pythonpojke@gmail.com>
2021-05-20 14:54:30 -0400

    pretty print mod

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ has_person_name
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 end
