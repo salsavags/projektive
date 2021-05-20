694be82910700da2e561bac4b804d1f77de2e764
Ian Johnson <pythonpojke@gmail.com>
2021-05-20 14:46:33 -0400

    db: update migs

--- db/migrate/20210520183949_create_projects.rb
+++ db/migrate/20210520183949_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration[6.1]
   def change
     create_table :projects do |t|
       t.string :title
-      t.decimal :donation_goal
+      t.decimal :donation_goal, default: 0
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
